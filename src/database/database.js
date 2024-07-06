1113f56592538f1fd364293341f7c09f89b0146e
eperezr <eddyperez007@hotmail.com>
2024-07-05 22:23:14 -0400

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -11,7 +11,7 @@ export const sequelize = new Sequelize(
     {
       host: process.env.DB_HOST,
       dialect: process.env.DB_DIALECT,
-      logging: console.log(),  
+      logging: console.log,  
      dialectOptions: {
         ssl: {
           require: true,
